7bbdf5786acc57f29504a75eac1871ec6f0e3670
condy <jhoncarlo.condy@gmail.com>
2025-05-09 22:22:01 +0800

    feat: add other skills sections with images

--- src/app/sections/skills.tsx
+++ src/app/sections/skills.tsx
@@ -8,6 +8,7 @@ import { Label } from '@radix-ui/react-label';
 import { skillType } from '@/types/skills';
 import frontendSkills from '@/data/skills/frontendSkills.json';
 import backendSkills from '@/data/skills/backendSkills.json';
+import otherSkills from '@/data/skills/otherSkills.json';
 
 const Skills = () => {
 	return (
@@ -127,7 +128,56 @@ const Skills = () => {
 							))}
 						</AnimatedGroup>
 					</TabsContent>
-					<TabsContent value='other'></TabsContent>
+					<TabsContent value='other'>
+						<AnimatedGroup
+							className='grid grid-cols-2 gap-4 p-12 md:grid-cols-3 lg:grid-cols-4 gap-x-30'
+							variants={{
+								container: {
+									visible: {
+										transition: {
+											staggerChildren: 0.05,
+										},
+									},
+								},
+								item: {
+									hidden: {
+										opacity: 0,
+										filter: 'blur(12px)',
+										y: -60,
+										rotateX: 90,
+									},
+									visible: {
+										opacity: 1,
+										filter: 'blur(0px)',
+										y: 0,
+										rotateX: 0,
+										transition: {
+											type: 'spring',
+											bounce: 0.3,
+											duration: 1,
+										},
+									},
+								},
+							}}
+						>
+							{otherSkills.map((skill: skillType) => (
+								<Card
+									key={1}
+									className='w-[100px] h-[100px] bg-white dark:bg-black shadow-md rounded-lg overflow-hidden items-center justify-center text-center'
+								>
+									<CardContent className='flex flex-col items-center justify-center gap-y-2'>
+										<Image
+											src={skill.skillPicture}
+											alt={skill.skillName}
+											width={30}
+											height={30}
+										/>
+										<Label className='text-sm'>{skill.skillName}</Label>
+									</CardContent>
+								</Card>
+							))}
+						</AnimatedGroup>
+					</TabsContent>
 				</Tabs>
 			</div>
 		</section>
